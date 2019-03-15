bf420267da6fce04ed27240208d8baca0008cc91
krishsa <krishnaveni.rl@gmail.com>
2019-03-15 09:24:26 -0500

    Changed the function name

--- examples/spread_function.js
+++ examples/spread_function.js
@@ -1,8 +1,8 @@
 const x = [1, 2];
-function sum3(a, b) {
+function sum(a, b) {
     return a + b;
 }
-const y = sum3(...x); // equivalent to sum3(1,2,3)
+const y = sum(...x); // equivalent to sum(1,2,3)
 console.log(y); // 6
 
 const f = [1, 2, 3];
@@ -11,11 +11,11 @@ const h = [...f, ...g];
 console.log(h);
 
 //.apply() takes arguments as an array
-const _apply = sum3.apply(0,x)
+const _apply = sum.apply(0,x)
 console.log(_apply)
 
 //.call() takes arguments as comma separated values
-const _call = sum3.call(0,...x)
+const _call = sum.call(0,...x)
 console.log(_call)
 
 
