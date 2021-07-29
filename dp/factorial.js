17e877171686662565e1a2e84851191cc67fce33
Senthil,Selvam <senthil.aru@gmail.com>
2021-07-29 05:33:51 -0500

    dynamic progs

---
+++ dp/factorial.js
@@ -0,0 +1,23 @@
+const fact = n => {
+    if (n === 1 || n === 0) return 1;
+    return fact(n - 1) + fact(n - 2)
+}
+const factMemoise = (n, cache) => {
+    if (n === 1 || n === 0) return 1;
+    if (!cache) cache = {};
+
+    if (cache[n]) return cache[n];
+    else cache[n] = fact(n - 1, cache) + fact(n - 2, cache);
+    
+    return cache[n];
+}
+const factorialBottomUp = n => {
+    let a = b = 1;
+    for (let i = 2; i <= n; i++) [a, b] = [b, a + b];
+    return b;
+}
+// 0 1 2 3 4 5 6
+// 1 1 2 3 5 8 13
+console.log(factorialBottomUp(5) == 8);
+console.log(fact(5) == 8);
+console.log(factMemoise(5) == 8);
