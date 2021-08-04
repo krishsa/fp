133f53946e6a9eaa5fba4d8f8d23a0f54611e014
Senthil,Selvam <senthil.aru@gmail.com>
2021-08-03 21:59:44 -0500

    twosum

--- dp/Test.java
+++ dp/Test.java
@@ -11,15 +11,7 @@ import java.util.stream.Stream;
 
 class Test {
     public static void main(String[] args) {
-        String s = "aaabbsbcs";
-        Map<Character, Long> result = s.chars().mapToObj(c -> (char) c)
-        .collect(
-            Collectors.groupingBy(Function.identity(), 
-                                Collectors.counting(), 
-                                Collectors.mapping(Function.identity(), new LinkedHashMap())) 
-        );
-        System.out.println("s :"+ result);
-
-        
+        String s = "aaabbsbcs";        
+        System.out.println("s :"+s);
     }
 }
--- dp/twoSum.js
+++ dp/twoSum.js
@@ -1,26 +1,15 @@
-const twoSum = (nums, target, start = 1, result = []) => {
-    let a = nums[start-1];
-    for (let i = start; i < nums.length; i++) {
-        let diff = target - nums[i];
-        console.log(" nums[" + i + "]:" + nums[i] + " diff :" + diff + " target :" + target);
-        if (diff === 0) result.push(i);
-        else if (diff > 0) return twoSum(nums, diff, start + 1, result);
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+ var twoSum = function(nums, target) {
+    let cache = [];
+    for(let i in nums){
+        let diff = target- nums[i];
+        if(cache[diff]) return [cache[diff], i];
+        cache[nums[i]] = i;
     }
-    return result;
+    return cache;
 };
-
-// const twoSumBottomUp = (nums, target, result=[]) => {
-//     let a = nums[0];
-//     if((a -target) === 0) return
-//     for (let i = 1; i< nums.length; i++){
-//         let diff = target- nums[i];
-//         console.log("diff :"+diff + " target :"+ target + " nums[" + i + "]:"+nums[i]);
-//         if(diff === 0) result.push(i);
-//         else if(diff > 0) return twoSum(nums, diff, result);
-//     }
-//     return result;
-// };
-
-console.log(twoSum([2, 7, 11, 15], 9));
-// console.log(twoSum([3,2,4], 6));
-// console.log(twoSum([3,3], 6));
